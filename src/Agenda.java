7b39768a3acf4a9850c174c7725d34c7f633b1f3
calin <calin.pana941@gmail.com>
2019-04-02 14:00:49 +0300

    have added a huge txt

--- src/Agenda.java
+++ src/Agenda.java
@@ -4,51 +4,51 @@ import java.util.Map;
 import java.util.Scanner;
 import java.util.TreeMap;
 
-public class Agenda {
-    Map<String, Contact> contacts = new TreeMap<>();
-    Scanner sc = new Scanner(System.in);
-
-    public void menu() {
-        System.out.println("Choose an option:" );
-        System.out.println("1. Create a backup file" );
-        System.out.println("2. View backup file details" );
-        System.out.println("3. Get a backup file" );
-        System.out.println("4. Delete a backup file" );
-    }
-
-    public void createBackupFile(Map<String, Contact> contacts) throws IOException {
-
-        File file = new File("Backups/backup_" + System.currentTimeMillis() + ".txt" );
-        file.getParentFile().mkdirs();
-
-        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
-
-    }
-
-    public void viewBackupFileDetails(Map<String, Contact> contacts) {
-        //afisez informatii despre backup-uri, am acces la date modified etc.
-        File folder = new File("C:\\Users\\JavaUser\\Jademy\\Backups" );
-        for (int i=0; i<folder.listFiles(); i++) {
-            System.out.println(i+1 + " " + folder.listFiles()[i].getName());
-            System.out.println(folder.listFiles()[i].lastModified());
-        }
-    }
-
-    public void getBackupFile(Map<String, Contact> contacts) {
-        System.out.println("Choose one of the backup files");
-        int option = sc.nextInt();
-        File folder = new File("C:\\Users\\JavaUser\\Jademy\\Backups" );
-        //try()
-        folder.listFiles()[option-1]
-
-
-        }
-
-        public void deleteBackupFile(Map<String,Contact> contacts){
-            System.out.println("Which one would you like to delete?");
-
-        }
-    }
+//public class Agenda {
+//    Map<String, Contact> contacts = new TreeMap<>();
+//    Scanner sc = new Scanner(System.in);
+//
+//    public void menu() {
+//        System.out.println("Choose an option:" );
+//        System.out.println("1. Create a backup file" );
+//        System.out.println("2. View backup file details" );
+//        System.out.println("3. Get a backup file" );
+//        System.out.println("4. Delete a backup file" );
+//    }
+//
+//    public void createBackupFile(Map<String, Contact> contacts) throws IOException {
+//
+//        File file = new File("Backups/backup_" + System.currentTimeMillis() + ".txt" );
+//        file.getParentFile().mkdirs();
+//
+//        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter(file)));
+//
+//    }
+//
+//    public void viewBackupFileDetails(Map<String, Contact> contacts) {
+//        //afisez informatii despre backup-uri, am acces la date modified etc.
+//        File folder = new File("C:\\Users\\JavaUser\\Jademy\\Backups" );
+//        for (int i=0; i<folder.listFiles(); i++) {
+//            System.out.println(i+1 + " " + folder.listFiles()[i].getName());
+//            System.out.println(folder.listFiles()[i].lastModified());
+//        }
+//    }
+//
+//    public void getBackupFile(Map<String, Contact> contacts) {
+//        System.out.println("Choose one of the backup files");
+//        int option = sc.nextInt();
+//        File folder = new File("C:\\Users\\JavaUser\\Jademy\\Backups" );
+//        //try()
+//        folder.listFiles()[option-1]
+//
+//
+//        }
+//
+//        public void deleteBackupFile(Map<String,Contact> contacts){
+//            System.out.println("Which one would you like to delete?");
+//
+//        }
+//    }
 
 
 
--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        Map<String, Contact> contacts = new TreeMap<>();
+        Map<String, Contact> contacts = new HashMap<>();
         //ca sa le sortez dupa lastName, trebuie sa fac un comparator pentru TreeMap
 
 //        try {
@@ -23,29 +23,29 @@ public class Main {
         //File f = new File("resources\\contacts\\contacts.txt");
         //System.out.println("Absolute path: " + f.getAbsolutePath());
 
-try readFile(Contacts.txt);
+//try readFile(Contacts.txt);
 
-//        try (BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\JavaUser\\Jademy\\Contacts.txt" ))) {
-//            String sCurrentLine = "";
-//
-//            String header = in.readLine();
-//
-//            while ((sCurrentLine = in.readLine()) != null) {
-//                String[] parts = sCurrentLine.split("," );
-//                // for (int i = 0; i < parts.length; i++) {
-//                // System.out.print(parts[i]);
-//                // System.out.println();
-//                // }
-//                String lastName = parts[0];
-//                String firstName = parts[1];
-//                String phoneNumber = parts[2];
-//                String email = parts[3];
-//                Contact contact = new Contact(firstName, lastName, phoneNumber, email);
-//                contacts.put(contact.getFullName(), contact);
-//            }
-//        } catch (IOException e) {
-//            System.out.println("The file doesn't exist" );
-//        }
+        try (BufferedReader in = new BufferedReader(new FileReader("C:\\Users\\test\\Jademy\\ContactList\\Contacts.txt" ))) {
+            String sCurrentLine = "";
+
+            String header = in.readLine();
+
+            while ((sCurrentLine = in.readLine()) != null) {
+                String[] parts = sCurrentLine.split("," );
+                // for (int i = 0; i < parts.length; i++) {
+                // System.out.print(parts[i]);
+                // System.out.println();
+                // }
+                String lastName = parts[0];
+                String firstName = parts[1];
+                String phoneNumber = parts[2];
+                String email = parts[3];
+                Contact contact = new Contact(firstName, lastName, phoneNumber, email);
+                contacts.put(contact.getFullName(), contact);
+            }
+        } catch (IOException e) {
+            System.out.println("The file doesn't exist" );
+        }
 
 //        try {
 //            Contact c3 = Contact.addContact();
@@ -116,7 +116,7 @@ try readFile(Contacts.txt);
 
 
         public static void writeFile (Map < String, Contact > contacts) throws IOException {
-            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\JavaUser\\Jademy\\Contacts.txt" )));
+            PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("C:\\Users\\test\\Jademy\\ContactList\\ContactsOut.txt" )));
             out.write("LastName,FirstName,PhoneNumber,Email" );
             out.println();
             for (Contact c : contacts.values()) {
